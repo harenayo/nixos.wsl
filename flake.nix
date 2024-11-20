135c8e5ab4a8f7ef471e14449fb55f31eb407636
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-11-20 14:37:51 +0000

    Add comment

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
     in
     {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        # https://github.com/nix-community/NixOS-WSL/blob/a6b9cf0b7805e2c50829020a73e7bde683fd36dd/flake.nix#L29-L74
         modules = [
           nixos-wsl.nixosModules.wsl
           (
@@ -33,9 +34,11 @@
         ];
       };
       apps.${system}.build = {
+        # https://github.com/numtide/flake-utils/blob/11707dc2f618dd54ca8739b309ec4fc024de578b/lib.nix#L193-L201
         type = "app";
         program =
           let
+            # https://nix-community.github.io/NixOS-WSL/building.html
             drv = self.nixosConfigurations.default.config.system.build.tarballBuilder;
           in
           "${drv}/bin/${drv.meta.mainProgram}";
